93426ecf160c70e756b9093c52d5d0fece88fa4c
NotCuteButNew <91822067+NotCuteButNew@users.noreply.github.com>
2021-10-03 16:52:15 +0800

    Add files via upload
    input i and j; output The JTH bit of the binary of the i.

---
+++ transfrom.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int Dec_to_Bin(int dec, int loc);//dec十进制，loc位置
+int dec_to_bin(int dec, int loc);
+
+int main()
+{
+	int i, j;
+	printf("Please input the value of i and j:\t");
+	scanf_s("%d%d", &i, &j);
+	printf("%d\n%d\n", Dec_to_Bin(i, j), dec_to_bin(i, j));
+	return 0;
+}
+
+int Dec_to_Bin(int dec, int loc)
+{
+	for (int i = 0; i < loc - 1; i++)
+		dec /= 2;
+	return dec % 2;
+}
+
+int dec_to_bin(int dec, int loc)
+{
+	dec >>= (loc - 1);
+	return dec & 1;
+}
